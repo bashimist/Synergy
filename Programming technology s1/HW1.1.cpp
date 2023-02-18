6f0d0fe6dde46c629c42292ac60a08066145451b
bashimist <andrimo@yandex.ru>
2023-02-18 14:09:50 +0300

    1

--- Programming technology s1/HW1.1.cpp
+++ Programming technology s1/HW1.1.cpp
@@ -1,4 +1,6 @@
-/*https://github.com/bashimist/Synergy/blob/main/Programming%20technology%20s1/HW1.1.cpp*/
+/*
+https://github.com/bashimist/Synergy/blob/main/Programming%20technology%20s1/HW1.1.cpp
+*/
 #include<stdio.h>
 
 main()
--- Programming technology s1/HW2.2.cpp
+++ Programming technology s1/HW2.2.cpp
@@ -1,3 +1,7 @@
+/*
+https://github.com/bashimist/Synergy/blob/main/Programming%20technology%20s1/HW2.2.cpp
+*/
+
 #include <stdio.h>
 
 main()
