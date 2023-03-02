cc2ccfa14c0be9d988edd572e51a8a26ec3933dd
bashimist <andrimo@yandex.ru>
2023-03-03 01:04:06 +0300

    hw3.8

---
+++ Programming technology s1/HW3.8.cpp
@@ -0,0 +1,47 @@
+/*
+
+*/
+#include <stdio.h>
+#define N 4
+
+main()
+{
+	int min;
+	int max;
+	int arr[N];
+	float k;
+	int i;
+	int n;
+	int sum;
+	printf("Enter the number of digits: ");
+	scanf("%i",&n);
+	for(i=0; i<n; i++)
+	{
+		printf("[%i] -> ", i);
+		scanf("%i",&arr[i]);
+	}
+	/*for (i = 0; i < n; i++)
+	{
+		printf("%i \n", arr[i]);
+	}*/
+	sum = 0;
+	for(i = 0; i<n; i++)
+	{
+		sum = sum + arr[i];
+	}
+	k = sum / n;
+	printf("The amount of the array: %i\n",sum);
+	printf("Average: %f\n",k);
+	max = arr[0];
+	min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (min > arr[i])
+			min = arr[i];
+			
+		if (max < arr[i])
+			max = arr[i];
+	}
+	printf("min value [%i]\n", min, arr[min]);
+	printf("max value [%i]\n", max, arr[max]);
+}
