9af88d713f40b27a2c1f7775cc0d1c439237049a
bashimist <andrimo@yandex.ru>
2023-03-02 20:32:27 +0300

    hw36

---
+++ Programming technology s1/HW3.6.1.cpp
@@ -0,0 +1,84 @@
+/*
+
+*/
+#include <stdio.h>
+
+main()
+{
+	int a=1;
+	while(a!=7)
+	{
+		printf("Enter the number: ");
+		scanf("%i",&a);
+		if(a%2 == 0 && a>10)
+		{
+			printf("The number is divided into 2 and more than 10\n");
+		}
+		else
+		{
+			if(a%3 == 0 && a>10)
+			{
+				printf("The number is divided into 3 and more than 10\n");
+			}
+			else
+			{
+				if(a>10)
+				{
+					printf("The number is more than 10\n");
+				}
+				else
+				{
+					if(a>7 && a<10 && a%2 == 0)
+					{
+						printf("The number is less than 10 and is divided by 2\n");
+					}
+					else
+					{
+						if(a>7 && a<10 && a%3 == 0)
+						{
+							printf("The number is less than 10 and is divided by 3\n");
+						}
+						else
+						{
+							if(a>7 && a%2 == 0)
+							{
+								printf("The number is greater and is divided by 2\n");
+							}
+							else
+							{
+								if(a>7 && a%3 == 0)
+								{
+									printf("The number is greater and is divided by 3\n");
+								}
+								else
+								{
+									if(a<7 && a%2 == 0)
+									{
+										printf("The number is less and is divided by 2\n");
+									}
+									else
+									{
+										if(a<7 && a%3 == 0)
+										{
+											printf("The number is less and is divided by 3\n");
+										}
+										else
+										{
+											if(a<7)
+											{
+												printf("The number is less\n");
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	printf("You guessed it! Congratulation!\n");
+	
+}
---
+++ Programming technology s1/HW3.6.cpp
@@ -0,0 +1,12 @@
+/*
+
+*/
+#include <stdio.h>
+
+main()
+{
+	for(int i=1;i<11;i++)
+	{
+		printf("%i*5=%i\n",i,i*5);
+	}
+}
