58ed7ea4a391b13cc46bf641116c5daa0d342a7c
bashimist <andrimo@yandex.ru>
2023-03-09 19:42:59 +0300

    Create HW4.4.cpp

---
+++ Programming technology s1/HW4.4.cpp
@@ -0,0 +1,42 @@
+/*
+https://github.com/bashimist/Synergy/blob/main/Programming%20technology%20s1/HW4.3.cpp
+*/
+#include <stdio.h>
+
+main()
+{
+	int mas[100];
+	int n;
+	
+	printf("kol el: ");
+	scanf("%i",&n);
+	
+	for(int i=0;i<n;i++)
+	{
+		scanf("%i",&mas[i]);
+	}
+	
+	int min,max;
+	min=max=mas[0];
+	
+	int sum=0;
+	
+	for(int i=0;i<n;i++)
+	{
+		if(mas[i]>max)
+		{
+			max=mas[i];
+		}
+		if(mas[i]<min)
+		{
+			min=mas[i];
+		}
+		
+		sum+=mas[i];
+	}
+	
+	
+	float sr=(float)sum/(float)n;
+	
+	printf("Sum=%i Sr=%f Min=%i Max=%i",sum,sr,min,max);
+}
