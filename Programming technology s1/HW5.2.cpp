a91a3aa87554b9fca727f46e91cef2b5b4ef372d
bashimist <andrimo@yandex.ru>
2023-03-11 09:29:14 +0300

    Create HW5.2.cpp

---
+++ Programming technology s1/HW5.2.cpp
@@ -0,0 +1,32 @@
+/*
+
+*/
+#include <stdio.h>
+
+main()
+{
+	int mas[100][100];
+	
+	int n,m;
+	printf("n= ");
+	scanf("%d",&n);
+	printf("m= ");
+	scanf("%d",&m);
+	
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<m;i++)
+		{
+			scanf("%i",&mas[j][i]);
+		}
+	}
+
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<m;i++)
+		{
+			printf("%3i",mas[j][i]);
+		}
+		printf("\n\n");
+	}
+}
