ad24d30db2ae7bff4a742edb4a8e2a17fc6bd4b3
bashimist <andrimo@yandex.ru>
2023-03-13 21:17:20 +0300

    Create HW6.4.cpp

---
+++ Programming technology s1/HW6.4.cpp
@@ -0,0 +1,27 @@
+/*
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+main()
+{
+	char str[100];
+	printf("Enter your word: ");
+	gets(str);
+	int q = strlen(str);
+	printf("Word without first symbol: ");
+	for(int i=0;i<strlen(str);i++)
+	{
+		str[i]=str[i+1];
+		printf("%c",str[i]);
+	}
+
+	printf("\n\n");
+	printf("Enter new word: ");
+	gets(str);
+	int c = strlen(str);
+	for(int i=c-1;i>-1;i--)
+	putchar(str[i]);
+
+}
